a1b909071ff14736541d4c9f72a59cdc52d62b04
Juanchi Parra <juanchiparra@gmail.com>
2025-09-01 14:04:03 -0400

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type WeekItem = {
+    week: number;
+    text: string;
+    style: string;
+    tooltip?: string;
+    image?: string;
+};
+
+export type SectionItem = {
+    title: string;
+    startDate: string;
+    endDate: string;
+};
